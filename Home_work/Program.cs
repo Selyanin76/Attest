801db7d7753d9c92c30fe8763dee7ed243daeaf6
Alexandr <fduch@ngs.ru>
2023-11-30 15:16:39 +0700

    Задачи промежуточной АТТЕСТАЦИИ

---
+++ Home_work/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Программа выводит все натуральные числа от N до 1
+
+*/ 
+
+Console.Clear();
+
+void DigitDiapas(int max, int start = 1)
+{
+    //System.Console.WriteLine();
+    if(start>max)
+    {
+        return ;
+    }
+    else
+    {
+        System.Console.Write($"{max}, " );
+        DigitDiapas(max-1, start);
+
+    }
+
+}
+DigitDiapas(8);
