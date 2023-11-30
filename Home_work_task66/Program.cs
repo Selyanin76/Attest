f867f6541b160ff2e44a6ae5b23223671d06199d
Alexandr <fduch@ngs.ru>
2023-11-30 15:20:40 +0700

    Аттстация

---
+++ Home_work_task66/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Программа принимает два значения M и N и выдает все натуральные числа в промежутке от  N до M
+
+*/
+
+Console.Clear();
+
+int Summ (int start, int max)
+{
+    
+    if(start==max)
+    {            
+        return start;
+    }    
+    else
+    {         
+     return start+Summ(start+1,max);           
+    }
+   
+}
+//System.Console.WriteLine(Summ(1,9));
+int result = Summ(1,9);
+System.Console.WriteLine($"Сумма всех чисел в заданом диапазоне равна {result}");
